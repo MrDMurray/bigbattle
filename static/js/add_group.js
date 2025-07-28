39a27cec5a3dfd0713a7c3e0da4387e2439fb4a9
Daniel Murray <daniel.murray@stjosephsrush.ie>
2025-07-28 20:26:43 +0100

    Add template saving and restore attack result label

---
+++ static/js/add_group.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('group-form');
+const loadBtn = document.getElementById('load-template-btn');
+const saveBtn = document.getElementById('save-template-btn');
+const select = document.getElementById('template-select');
+
+if (loadBtn && select && form) {
+  loadBtn.addEventListener('click', () => {
+    const opt = select.options[select.selectedIndex];
+    if (!opt || !opt.dataset.name) return;
+    form.elements['name'].value = opt.dataset.name;
+    form.elements['ac'].value = opt.dataset.ac;
+    form.elements['hp'].value = opt.dataset.hp;
+    form.elements['count'].value = opt.dataset.count;
+    form.elements['damage_die'].value = opt.dataset.damage_die;
+    form.elements['damage_bonus'].value = opt.dataset.damage_bonus;
+    form.elements['attack_name'].value = opt.dataset.attack_name;
+    form.elements['attack_bonus'].value = opt.dataset.attack_bonus;
+  });
+}
+
+if (saveBtn && form) {
+  saveBtn.addEventListener('click', () => {
+    const fd = new FormData(form);
+    fetch('/save_template', { method: 'POST', body: fd })
+      .then(() => window.location.reload());
+  });
+}
